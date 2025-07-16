cbd7036b44dbdcc7737d3bc90055622ec76c4644
gioLomb <151877251+gioLomb@users.noreply.github.com>
2025-07-16 12:31:46 +0200

    Update pas.h

--- Web_Server_Arduino/pas.h
+++ Web_Server_Arduino/pas.h
@@ -1,4 +1,2 @@
-//#define SECRET_SSID "Wind3 HUB - A8B490"
-//#define SECRET_PASS "5765tfazzp33kx9p"
-#define SECRET_SSID "realme C21"
-#define SECRET_PASS "sonoproprioio"
+#define SECRET_SSID [your_SSID]
+#define SECRET_PASS [your_password]
